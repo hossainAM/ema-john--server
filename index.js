b3212aca1ebfffcd992039d4bd068ca232b84202
Amir Hossain <amir508_du@yahoo.com>
2022-04-24 05:21:54 +0600

    ema-john server initialized

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors);
+app.use(express.json());
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Ema John server is running');
+});
+
+//Dynamic port
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
