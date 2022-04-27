e034933d0109e1d6f8ad28e2e93e5caa2efb733b
Amir Hossain <amir508_du@yahoo.com>
2022-04-27 07:41:44 +0600

    post api added to load cart products

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const app = express();
 const cors = require('cors');
 const {
     MongoClient,
-    ServerApiVersion
+    ServerApiVersion,
+    ObjectId
 } = require('mongodb');
 const res = require('express/lib/response');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 //Connect with mongoDB
@@ -24,13 +25,41 @@ async function run () {
     try{
         await client.connect();
         const productCollection = client.db('emajohn').collection('product');
-        // console.log('DB is connected')
+    
+        //api endpoint to load products page and size wise
         app.get('/product', async (req, res) => {
+            // console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
             const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if(page || size) {
+                products = await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+                products = await cursor.toArray();
+
+            }
+            res.send(products);
+        });
+
+        //api end point to to get product count
+        app.get('/productcount', async (req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count})//res.json can also be used if count as a variable is to sent, as count is not an json object;
+        });
+
+        //api endpoint by productkeys to load cart products
+        app.post('/productbykeys', async (req, res) => {
+            const keys = req.body;
+            const ids = keys.map(id => ObjectId(id))
+            const query = {_id: {$in: ids}};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
-        });
+            // console.log(keys);
+        })
     }
     finally{
         // client.close();
